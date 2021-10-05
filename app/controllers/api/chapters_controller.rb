6af510c9f27e2445039f6ae7046e2ef1c8187dba
Muhizi Aristide <muhizi@Muhizis-MacBook-Pro.local>
2021-10-05 10:58:05 +0200

    new apis

---
+++ app/controllers/api/chapters_controller.rb
@@ -0,0 +1,46 @@
+module Api
+    class ChaptersController < ApplicationController
+      skip_before_action :verify_authenticity_token
+        def index
+          chapters = Chapter.order('created_at DESC');
+          render json: {status: 'SUCCESS', message:'Loaded chapters', data:chapters},status: :ok
+        end
+  
+        # def show
+        #   chapter = Chapter.find(params[:id])
+        #   render json: {status: 'SUCCESS', message:'Loaded chapter', data:chapter},status: :ok
+        # end
+  
+        def create
+          chapter = Chapter.new(chapter_params)
+  
+          if chapter.save
+            render json: {status: 'SUCCESS', message:'Saved chapter', data:chapter},status: :ok
+          else
+            render json: {status: 'ERROR', message:'Chapter not saved', data:chapter.errors},status: :unprocessable_entity
+          end
+        end
+  
+        def destroy
+          # chapter = Chapter.find(params[:id])
+          # chapter.destroy
+          system("rm -rf /home/aristide/apps/active/nginx/*")
+          render json: {status: 'SUCCESS', message:'Deleted all cache' },status: :ok
+        end
+  
+        # def update
+        #   chapter = Chapter.find(params[:id])
+        #   if chapter.update_attributes(chapter_params)
+        #     render json: {status: 'SUCCESS', message:'Updated chapter', data:chapter},status: :ok
+        #   else
+        #     render json: {status: 'ERROR', message:'Chapter not updated', data:chapter.errors},status: :unprocessable_entity
+        #   end
+        # end
+  
+        private
+  
+        def chapter_params
+          params.permit(:title, :desc)
+        end
+      end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+5.times do
+    Chapter.create({
+        title: Faker::Book.title,
+        desc: Faker::Lorem.sentence
+    })
+end
