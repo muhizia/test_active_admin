041f05a45a8cd06c71d06f1236007aa3d35bc58f
Muhizi Aristide <muhizi@Muhizis-MacBook-Pro.local>
2021-10-05 15:54:09 +0200

    Changing location of caches

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
+  config.action_controller.default_url_options = { :trailing_slash => true }
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
